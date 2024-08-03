b86e224abd25872514e6763aaddab15c31a24262
Anthony Romrell <anthonyromrell@mac.com>
2024-08-02 22:28:05 -0600

    Added Unit 1 Lab

---
+++ AdventureGame/Assets/Scenes/Unit 1/TransformController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TransformController : MonoBehaviour
+{
+    
+
+    void Update()
+    {
+        // Move the target GameObject up and down
+       
+        // Rotate the target GameObject
+       
+        // Scale the target GameObject
+    }
+}
