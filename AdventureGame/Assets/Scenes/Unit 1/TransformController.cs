df646e94cd15343c7f566ac8f2cd523478a8e966
Anthony Romrell <anthonyromrell@mac.com>
2024-08-29 11:08:17 -0600

    Transform

--- AdventureGame/Assets/Scenes/Unit 1/TransformController.cs
+++ AdventureGame/Assets/Scenes/Unit 1/TransformController.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
-    
-
-    void Update()
+    private void Update()
     {
-        // Move the target GameObject up and down
-       
+        // Move the target GameObject
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = new Vector3(0, x, 0);
+        transform.position = p;
+
         // Rotate the target GameObject
-       
-        // Scale the target GameObject
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
     }
 }
