76ed36554fca009c289cf0f5cbb34723ec8cd14f
tsp300 <goshdarnroxxarne@gmail.com>
2025-03-10 14:02:13 -0600

    Challenge 8

--- AdventureGame/Assets/Scripts/TextUpdate.cs
+++ AdventureGame/Assets/Scripts/TextUpdate.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using System.Globalization;
+using System.Collections;
 
 [RequireComponent(typeof(TextMeshProUGUI))]
 
@@ -9,14 +10,29 @@ public class TextUpdate : MonoBehaviour
     private TextMeshProUGUI textObj;
     public SimpleInt dataObj;
 
+    private int i = 20;
+    private RectTransform rectTrans;
+
     private void Start()
     {
         textObj = GetComponent<TextMeshProUGUI>();
+        rectTrans = GetComponent<RectTransform>();
         UpdateWithIntData();
     }
 
     public void UpdateWithIntData()
     {
         textObj.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+        StartCoroutine(shaking());
+    }
+
+    private IEnumerator shaking()
+    {
+        rectTrans.rotation = Quaternion.Euler(0,0,i);
+        yield return new WaitForSeconds(0.1f);
+        rectTrans.rotation = Quaternion.Euler(0,0,-i);
+        yield return new WaitForSeconds(0.1f);
+        rectTrans.rotation = Quaternion.Euler(0, 0, 0);
+        i *= -1;
     }
 }
