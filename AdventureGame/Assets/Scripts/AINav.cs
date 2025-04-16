ada41f4b842a410fb957df1bfc3461600a36960d
tsp300 <goshdarnroxxarne@gmail.com>
2025-04-16 10:57:53 -0600

    Challenge 12

--- AdventureGame/Assets/Scripts/AINav.cs
+++ AdventureGame/Assets/Scripts/AINav.cs
@@ -5,20 +5,26 @@ using UnityEngine.AI;
 
 public class AINav : MonoBehaviour
 {
-    public Transform target;
+    public Transform[] waypoints;
+    private int currentWaypointIndex;
     private NavMeshAgent agent;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
+        if (waypoints.Length > 0)
+        {
+            agent.SetDestination(waypoints[0].position);
+        }
     }
 
     private void Update()
     {
-        if (target != null)
+        if (agent.remainingDistance < agent.stoppingDistance)
         {
-            agent.SetDestination(target.position);
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            agent.SetDestination(waypoints[currentWaypointIndex].position);
         }
     }
 }
