d270064d155add17cde8c8295e17fb581b2e533b
tsp300 <goshdarnroxxarne@gmail.com>
2025-04-26 15:57:17 -0600

    Lab 14

--- AdventureGame/Assets/Scripts/CameraBrrr.cs
+++ AdventureGame/Assets/Scripts/CameraBrrr.cs
@@ -17,19 +17,21 @@ public class CameraBrrr : MonoBehaviour
     {
         pos = transform.position;
 
-        pos.x += i;
+        pos.x += i;             // position changing shenanigans
         pos.y += i;
         transform.position = pos;
 
         yield return new WaitForSeconds(0.05f);
 
-        pos.x -= i * 2;
+        pos = transform.position;
+        pos.x -= i * 2;          // position changing shenanigans again
         pos.y -= i * 2;
         transform.position = pos;
 
         yield return new WaitForSeconds(0.05f);
 
-        pos.x += i;
+        pos = transform.position;
+        pos.x += i;              // position changing back
         pos.y += i;
         transform.position = pos;
         i *= -1;
